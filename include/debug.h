60ba3199d28cd2d019ffa3e9e12309330b1bf1dc
R.Chavignat <r.chavignat@gmail.com>
2015-12-12 04:02:01 +0100

    Add UNREACHABLE macro

--- include/debug.h
+++ include/debug.h
@@ -2,11 +2,13 @@
 #define DEBUG_H
 
 #ifdef DEBUG
-#define DBG(p) p
-#ifndef YYDEBUG
-#define YYDEBUG 1
+    #define DBG(p) p
+    #ifndef YYDEBUG
+        #define YYDEBUG 1
+    #endif
 #endif
-#endif
+
+#define UNREACHABLE() (fprintf(stderr, "Reached unreachable code, bailing"), exit(1), 0)
 
 #ifndef DBG
 #define DBG(p)
